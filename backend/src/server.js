5569a137227f1bc80004756c3309f45c478b3346
varun <varunkumargangipelli@gmail.com>
2026-05-19 23:15:45 +0530

    small edit

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:3000",
-      "https://YOUR-VERCEL-URL.vercel.app"
+      "https://sync-up-coaching-feed-application.vercel.app"
     ]
   },
 });
